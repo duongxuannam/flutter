ba95b7bded212fac3aed04d6ca8fdbfc03c86088
Nam <duongxuannam1995@gmail.com>
2020-01-25 06:20:23 +0700

    init

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/image_01.png",
+  "assets/image_02.jpg",
+  "assets/image_03.jpg",
+  "assets/image_04.jpg",
+];
+
+List<String> title = [
+  "Fallen Angle",
+  "Ky Kinh muon nam",
+  "Song gio cau truong",
+  "Khi em co don"
+];
